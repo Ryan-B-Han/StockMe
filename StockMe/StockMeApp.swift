3ab06fc54ba2790dc7f38f02bd9eb66a1b4230cc
Ryan-B-Han <103276235+Ryan-B-Han@users.noreply.github.com>
2022-04-08 16:11:58 -0700

    chor: convert to swiftUI (#1)
    Co-authored-by: Ryan Han <trick14@gmail.com>

---
+++ StockMe/StockMeApp.swift
@@ -0,0 +1,20 @@
+//
+//  StockMeApp.swift
+//  StockMe
+//
+//  Created by BYUNGKI HAN on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct StockMeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
