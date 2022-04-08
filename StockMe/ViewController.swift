02708af0f577ad4c297b31b801bf1a924b1fea61
Ryan Han <trick14@gmail.com>
2022-04-08 10:54:27 -0700

    chor: Initial Commit

---
+++ StockMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockMe
+//
+//  Created by BYUNGKI HAN on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
